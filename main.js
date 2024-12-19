7413ecfbb70c91f2564fe012d890816846978243
azizbek2510 <azizbekaliyev1011@gmail.com>
2024-12-19 22:14:30 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+
+var ism = prompt("Ismingizni kiriting");
+console.log(ism);
+
+var familiya = prompt("Familiyangizni kiriting");
+console.log(familiya);
+
+var yosh = prompt("Yoshingizni kiriting");
+console.log(yosh);
+
+var soha = prompt("Sohangizni kiriting");
+console.log(soha);
+
+var tuman = prompt("Tumaningizni (Район) kiriting");
+console.log(tuman);
+
+
